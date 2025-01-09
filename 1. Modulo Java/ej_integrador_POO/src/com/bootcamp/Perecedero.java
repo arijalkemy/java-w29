5585fb70c9c3171254d08cf45de1683c731649bf
GuidoLandoni <guidolandoni97@gmail.com>
2025-01-09 14:31:59 -0300

    Agrego ejercicio integrador POO

---
+++ 1. Modulo Java/ej_integrador_POO/src/com/bootcamp/Perecedero.java
@@ -0,0 +1,44 @@
+package com.bootcamp;
+
+public class Perecedero extends Producto {
+    private int diasPorCaducar;
+
+    public Perecedero(String nombre, Double precio, int diasPorCaducar) {
+        super(nombre, precio);
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public String toString() {
+        return "Perecedero{" +
+                "diasPorCaducar=" + diasPorCaducar +
+                '}';
+    }
+
+    @Override
+    public double calcular(int cantidadDeProductos) {
+        double precioFinal = super.calcular(cantidadDeProductos);
+
+        if (diasPorCaducar == 1) {
+            precioFinal -= precioFinal / 4;
+        }
+
+        if (diasPorCaducar == 2) {
+            precioFinal -= precioFinal / 3;
+        }
+
+        if (diasPorCaducar == 3) {
+            precioFinal -= precioFinal / 2;
+        }
+
+        return precioFinal;
+    }
+}
