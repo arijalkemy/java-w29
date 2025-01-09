fc7153033cc4b0e6b5577fbfa18f9995ee969ad8
ccmendezt <ccmendezt@udistrital.edu.co>
2025-01-09 15:13:34 -0500

    feat: added imc exercise

---
+++ 1. Modulo Java/imc/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        Persona juan = new Persona("Juan", 37, "2948848");
+        Persona josefina = new Persona("Josefina", 20, "202020", 57.1, 1.80);
+
+        josefina.calcularPeso();
+        System.out.println(verificarEdad(josefina.esMayorDeEdad()));
+        System.out.println(verificarIMC(josefina.calcularPeso()));
+        System.out.println(josefina);
+    }
+
+    private static String verificarIMC(int valor){
+        return switch (valor){
+            case -1 -> "Bajo peso";
+            case 0 -> "Peso normal";
+            case 1 -> "Sobrepeso";
+            default -> "Valor no válido";
+        };
+    }
+
+    private static String verificarEdad(boolean valor){
+        if (valor){
+            return "Es mayor de edad";
+        } else {
+            return "Es menor de edad";
+        }
+    }
+
+}
