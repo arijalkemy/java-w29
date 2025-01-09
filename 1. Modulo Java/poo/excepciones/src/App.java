cd0588964e2139168fe9309b052979fac61ff45c
brarellano_meli <brayan.arellano@mercadolibre.com.co>
2025-01-09 17:08:46 -0500

    POO - P2 - VIVO

---
+++ 1. Modulo Java/poo/excepciones/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 0, b = 300;
+        try {
+            if (a == 0) {
+                throw new IllegalArgumentException("No se puede dividir por cero");
+            }
+            int cociente = b / a;
+            System.out.println("El cociente es: " + cociente);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+
+}
---
+++ 1. Modulo Java/poo/perecederos/src/Distribuidora.java
@@ -0,0 +1,15 @@
+public class Distribuidora {
+    public static void main(String[] args) {
+        Producto[] productos = new Producto[4];
+        productos[0] = new Perecedero("Leche", 50.0, 2);
+        productos[1] = new Perecedero("Queso", 80.0, 1);
+        productos[2] = new NoPerecedero("Arroz", 40.0, "Grano");
+        productos[3] = new NoPerecedero("Aceite", 100.0, "Líquido");
+        double precioTotal = 0;
+        for (Producto producto : productos) {
+            precioTotal += producto.calcular(5);
+            System.out.println(producto);
+        }
+        System.out.println("Precio total al vender 5 productos de cada tipo: " + precioTotal);
+    }
+}
