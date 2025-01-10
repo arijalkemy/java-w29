49a7e247e69e7a154707844dd8943eaf83ebce4a
Juan Feo <juan.feo@mercadolibre.com.co>
2025-01-10 15:42:52 -0500

    Subieno SPRING P2 VIVO

---
+++ 2. Modulo Spring/SPRING-P2-VIVO/src/main/java/org/example/springp2vivo/SpringP2VivoApplication.java
@@ -0,0 +1,16 @@
+package org.example.springp2vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringP2VivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringP2VivoApplication.class, args);
+    }
+
+
+
+
+}
---
+++ 2. Modulo Spring/SPRING-P2-VIVO/src/test/java/org/example/springp2vivo/SpringP2VivoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springp2vivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringP2VivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
