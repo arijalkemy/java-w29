b1d16ada32cf902f8169e2616b621a10bdb7f27b
Angelo Daniel Padron <padron891@gmail.com>
2025-01-10 14:41:05 -0300

    add exercise

---
+++ 1. Modulo Java/POO - P2 - VIVO/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Producto[] productos = {
+                new Perecedero("Leche", 10.0, 1),
+                new Perecedero("Huevos", 20.0, 2),
+                new NoPerecedero("Lentejas", 15.0, "Grano"),
+                new NoPerecedero("Arvejas", 25.0, "Grano"),
+                new Perecedero("Vacio", 30.0, 5),
+        };
+
+        double total = 0;
+        int cantidadProducto = 5;
+
+        for (Producto producto : productos) {
+            total += producto.calcular(cantidadProducto);
+        }
+
+        System.out.println("Total: " + Math.round(total));
+
+
+
+    }
+}
