6533109fb3f7138a1900b679a31dae8a35ea0939
sechacon_meli <sebastian.schacon@mercadolibre.com.co>
2025-01-10 10:55:46 -0500

    add: Exercise factorial

---
+++ 2. Modulo Spring/Factorial/src/main/java/org/example/factorial/FactorialApplication.java
@@ -0,0 +1,13 @@
+package org.example.factorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactorialApplication.class, args);
+    }
+
+}
---
+++ 2. Modulo Spring/Factorial/src/test/java/org/example/factorial/FactorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.factorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
