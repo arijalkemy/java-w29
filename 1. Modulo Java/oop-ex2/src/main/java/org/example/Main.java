373e9e9b431f7d54d7da169d091647ffa0cd1b84
Luis Manuel Jaimes Silva <luis.jaimes@mercadolibre.com.co>
2025-01-10 22:32:32 -0500

    Practica de estructura de datos, extraer máxima temperatura de un listado de ciudades

---
+++ 1. Modulo Java/oop-ex2/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        new Distribuidora();
+    }
+}
