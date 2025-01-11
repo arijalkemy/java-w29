1798588e8d85d56e6f07a8204b73f9c601145bc2
Luis Manuel Jaimes Silva <luis.jaimes@mercadolibre.com.co>
2025-01-10 21:42:33 -0500

    Ejercicio de POO, mostrando datos de un automóvil instanciado una clase

---
+++ 1. Modulo Java/Practica2PooAutomovil/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Automovil auto = new Automovil("Ford", "Azul", 20);
+        System.out.println(auto.mostrarMarcaYColor());
+    }
+}
