c65f8ed40bd355b4ad799a29937c7692373c7436
Luis Manuel Jaimes Silva <luis.jaimes@mercadolibre.com.co>
2025-01-10 21:59:02 -0500

    Ejercicio práctico de POO, manejo de exepciones

---
+++ 1. Modulo Java/Practica5PooExceptions/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Ejercicio1();
+    }
+
+    public static void Ejercicio1() {
+        PracticaExcepciones prEx = new PracticaExcepciones(0, 2);
+        prEx.execute();
+    }
+}
