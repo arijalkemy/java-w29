a9575b3df9ea2c4f30240660c04170ec936e9858
Carolina Comba <111009258+carolinacomba@users.noreply.github.com>
2025-01-12 14:03:28 -0300

    Update NoPerecedero.java

--- 1. Modulo Java/POO - P2 - VIVO/ejercicio_2/src/main/java/org/meli/models/NoPerecedero.java
+++ 1. Modulo Java/POO - P2 - VIVO/ejercicio_2/src/main/java/org/meli/models/NoPerecedero.java
@@ -3,26 +3,9 @@ package org.meli.models;
 public class NoPerecedero extends Producto {
     private String tipo;
 
-    public NoPerecedero(String nombre, double precio, String tipo) {
+    public NoPerecedero(String nombre, Double precio, String tipo) {
         super(nombre, precio);
         this.tipo = tipo;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    @Override
-    public String toString() {
-        return "NoPerecedero{" +
-                "nombre='" + getNombre() + '\'' +
-                ", precio=" + getPrecio() +
-                ", tipo='" + tipo + '\'' +
-                '}';
     }
 }
 
