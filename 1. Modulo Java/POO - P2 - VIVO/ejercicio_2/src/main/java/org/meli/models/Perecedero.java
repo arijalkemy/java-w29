d8c23c272f643875965155921dc6ea3c420183b1
Carolina Comba <111009258+carolinacomba@users.noreply.github.com>
2025-01-12 14:03:09 -0300

    Update Perecedero.java

--- 1. Modulo Java/POO - P2 - VIVO/ejercicio_2/src/main/java/org/meli/models/Perecedero.java
+++ 1. Modulo Java/POO - P2 - VIVO/ejercicio_2/src/main/java/org/meli/models/Perecedero.java
@@ -1,46 +1,23 @@
 package org.meli.models;
 
 public class Perecedero extends Producto {
-    private int diasPorCaducar;
+    private Integer diasPorCaducar;
 
-    public Perecedero(String nombre, double precio, int diasPorCaducar) {
+    public Perecedero(String nombre, Double precio, Integer diasPorCaducar) {
         super(nombre, precio);
         this.diasPorCaducar = diasPorCaducar;
     }
 
-    public int getDiasPorCaducar() {
-        return diasPorCaducar;
-    }
-
-    public void setDiasPorCaducar(int diasPorCaducar) {
-        this.diasPorCaducar = diasPorCaducar;
-    }
-
-    @Override
-    public double calcular(int cantidadDeProductos) {
-        double precioFinal = super.calcular(cantidadDeProductos);
-
-        switch (diasPorCaducar) {
-            case 1:
-                precioFinal /= 4;
-                break;
-            case 2:
-                precioFinal /= 3;
-                break;
-            case 3:
-                precioFinal /= 2;
-                break;
-        }
-        return precioFinal;
-    }
-
-    @Override
-    public String toString() {
-        return "Perecedero{" +
-                "nombre='" + getNombre() + '\'' +
-                ", precio=" + getPrecio() +
-                ", diasPorCaducar=" + diasPorCaducar +
-                '}';
+    public Double calcular(Integer diasPorCaducar){
+        Double precio = this.getPrecio();
+        if(diasPorCaducar==1)
+            return precio*0.25;
+        else if(diasPorCaducar==2)
+            return precio*0.33;
+        else if(diasPorCaducar==3)
+            return precio*0.5;
+        else
+            return precio;
     }
 }
 
