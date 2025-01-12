9b0cc9d645e7019c12f816a797ea94e40545c1b2
Carolina Comba <111009258+carolinacomba@users.noreply.github.com>
2025-01-12 14:00:16 -0300

    Update Main.java

--- 1. Modulo Java/POO - P2 - VIVO/ejercicio_2/src/main/java/org/meli/Main.java
+++ 1. Modulo Java/POO - P2 - VIVO/ejercicio_2/src/main/java/org/meli/Main.java
@@ -6,17 +6,25 @@ import org.meli.models.Producto;
 
 public class Main {
     public static void main(String[] args) {
-        Producto[] productos = new Producto[3];
-        productos[0] = new Perecedero("Leche", 2.0, 2);
-        productos[1] = new NoPerecedero("Arroz", 1.5, "Alimento seco");
-        productos[2] = new Perecedero("Manzana", 1.0, 1);
+        Producto[] productos = new Producto[10];
+        productos[0] = new NoPerecedero("Arroz", 200.0, "sdfsf");
+        productos[1] = new Perecedero("Pollo", 400.0, 2);
+        productos[2] = new NoPerecedero("Legumbres", 250.0, "dfdsf");
+        productos[3] = new Perecedero("Carne", 700.0, 1);
+        productos[4] = new NoPerecedero("Fideos", 300.0, "sdssdf");
+        productos[5] = new Perecedero("Sushi", 1000.0, 10);
+        productos[6] = new NoPerecedero("Garbanzos", 250.0, "saddf");
+        productos[7] = new Perecedero("Bananas", 100.0, 30);
+        productos[8] = new Perecedero("Manzanas", 200.0, 5);
+        productos[9] = new NoPerecedero("Vino", 1000.0, "Bebida");
 
-        double precioTotal = 0;
+        Double resultadoTotal = 0.0;
+
         for (Producto producto : productos) {
-            precioTotal += producto.calcular(5);
+            resultadoTotal += producto.calcular(5);
         }
 
-        System.out.println("Precio total al vender 5 unidades de cada producto: $" + precioTotal);
+        System.out.println("Precio total al vender 5 unidades de cada producto: $" + resultadoTotal);
 
         System.out.println("\nDetalles de los productos:");
         for (Producto producto : productos) {
