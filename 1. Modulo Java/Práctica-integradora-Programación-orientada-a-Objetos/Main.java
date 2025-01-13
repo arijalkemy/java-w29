e313a03817b76fae868d9e811764c5e318f7a0ba
EFP210 <edwinpinilla125@gmail.com>
2025-01-12 20:58:01 -0500

    ejercicio IMC

--- 1. Modulo Java/Práctica-integradora-Programación-orientada-a-Objetos/Main.java
+++ 1. Modulo Java/Práctica-integradora-Programación-orientada-a-Objetos/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        //Persona persona1 = new Persona();
+        Persona persona2 = new Persona("Adrian", 24, "100374");
+        Persona persona3 = new Persona("Rossi", 43, "464646", 62.0, 1.62);
+        //persona1.calcularIMC();
+        //persona2.calcularIMC();
+        System.out.println(persona3.calcularIMC());
+        //persona1.esMayorDeEdad();
+        //persona2.esMayorDeEdad();
+        System.out.println(persona3.esMayorDeEdad());
+
+    }
 }
--- 1. Modulo Java/Práctica-integradora-Programación-orientada-a-Objetos/Persona.java
+++ 1. Modulo Java/Práctica-integradora-Programación-orientada-a-Objetos/Persona.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Persona {
+public class Persona {
+    public String Nombre;
+    public Integer Edad;
+    public String Dni;
+    public Double Peso;
+    public Double Altura;
+
+    public Persona() {}
+
+    public Persona(String nombre, Integer edad, String dni) {
+        Nombre = nombre;
+        Edad = edad;
+        Dni = dni;
+    }
+
+    public Persona(String nombre, Integer edad, String dni, Double peso, Double altura) {
+        Nombre = nombre;
+        Edad = edad;
+        Dni = dni;
+        Peso = peso;
+        Altura = altura;
+    }
+
+    public Double calcularIMC() {
+        if (Peso == null || Altura == null || Altura == 0) {
+            throw new IllegalArgumentException("Peso y Altura deben tener valores válidos.");
+        }
+
+        Double imc = Peso / (Altura * Altura);
+
+        if (imc < 20) {
+            return -1.0;
+        } else if (imc >= 20 && imc <= 25) {
+            return 0.0;
+        } else {
+            return 1.0;
+        }
+    }
+
+    public Boolean esMayorDeEdad() {
+        if (Edad == null) {
+            throw new IllegalArgumentException("Edad debe tener un valor válido.");
+        }
+
+        return Edad >= 18;
+    }
 }
