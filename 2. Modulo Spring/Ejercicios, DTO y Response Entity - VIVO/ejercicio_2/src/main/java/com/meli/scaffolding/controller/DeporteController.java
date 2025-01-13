6545041b141d7770c6a210ecafff41a280535f73
carolinacomba <carolinacomba42@gmail.com>
2025-01-13 14:46:10 -0300

    Update DeporteController.java

--- 2. Modulo Spring/Ejercicios, DTO y Response Entity - VIVO/ejercicio_2/src/main/java/com/meli/scaffolding/controller/DeporteController.java
+++ 2. Modulo Spring/Ejercicios, DTO y Response Entity - VIVO/ejercicio_2/src/main/java/com/meli/scaffolding/controller/DeporteController.java
@@ -2,6 +2,8 @@ package com.meli.scaffolding.controller;
 
 import com.meli.scaffolding.models.Deporte;
 import com.meli.scaffolding.models.Persona;
+import com.meli.scaffolding.models.PersonaDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,37 +16,37 @@ public class DeporteController {
     List<Deporte> deportes = new ArrayList<>(
             List.of(new Deporte("Voley", 1),
                     new Deporte("Tenis", 1),
-                    new Deporte("Futbol", 1),
+                    new Deporte("Futbol", 3),
                     new Deporte("Basquet", 1),
-                    new Deporte("Padel", 1),
+                    new Deporte("Padel", 3),
                     new Deporte("Natacion", 1),
                     new Deporte("Jockey", 1),
-                    new Deporte("Rugby", 1),
-                    new Deporte("Danza", 2),
+                    new Deporte("Rugby", 3),
                     new Deporte("Futbol Americano", 2),
                     new Deporte("Handball", 2),
                     new Deporte("Ping Pong", 2))
     );
 
-    List<Persona> personas = new ArrayList<>(
-            List.of(new Persona("Carolina", "Comba", 21),
-                    new Persona("Francisco", "Comba", 14),
-                    new Persona("Adriana", "Nivello", 51),
-                    new Persona("Trinidad", "Comba", 26),
-                    new Persona("Juan", "Perez", 21),
-                    new Persona("María", "Rodríguez", 20),
-                    new Persona("Pedro", "Gomez", 30))
+    List<PersonaDTO> personaDTOS = new ArrayList<>(
+            List.of(new PersonaDTO("Carolina", "Comba", "Tenis"),
+                    new PersonaDTO("Alejandro", "Díaz", "Padel"),
+                    new PersonaDTO("Elias", "Juarez", "Voley"))
     );
 
     @GetMapping("/findSports")
-    public List<Deporte> findSports() {
-        return deportes;
+    public ResponseEntity<List<Deporte>> findSports() {
+        return ResponseEntity.ok(deportes);
     }
 
     @GetMapping("/findSports/{name}")
-    public Integer findSports(@PathVariable String name) {
+    public ResponseEntity<Integer> findSports(@PathVariable String name) {
         Deporte d = deportes.stream().filter(deporte -> deporte.getNombre().equals(name)).findFirst().orElse(null);
-        return d.getNivel();
+        return ResponseEntity.ok(d.getNivel());
+    }
+
+    @GetMapping("/findSportsPersons")
+    public List<PersonaDTO> findSportsPersons() {
+        return personaDTOS;
     }
 
 
