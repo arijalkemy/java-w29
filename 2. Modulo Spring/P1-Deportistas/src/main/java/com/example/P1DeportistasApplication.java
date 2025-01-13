cf16e3f75be39c38fb97c56e58301618e56bee83
dyhurtado_meli <dylan.hurtado@mercadolibre.com>
2025-01-13 17:16:48 -0300

    Ejercicio Deportistas

---
+++ 2. Modulo Spring/P1-Deportistas/src/main/java/com/example/P1DeportistasApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1DeportistasApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(P1DeportistasApplication.class, args);
+}
+}
