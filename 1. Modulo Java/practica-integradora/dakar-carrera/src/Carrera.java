cd9279b0ad05439f5fbebf3bfcd05bf39da81277
ccmendezt <ccmendezt@udistrital.edu.co>
2025-01-13 14:28:38 -0500

    feat: added dakar-carrera exercise

---
+++ 1. Modulo Java/practica-integradora/dakar-carrera/src/Carrera.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrera {
+    private Double distancia;
+    private Double premioEnDolares;
+    private String nombre;
+    private Integer cantidadDeVehiculosPermitidos;
+    private List<Vehiculo> vehiculos;
+    private SocorristaAuto socorristaAuto;
+    private SocorristaMoto socorristaMoto;
+
+    public Carrera(Double distancia, Double premioEnDolares, String nombre, Integer cantidadDeVehiculosPermitidos) {
+        this.distancia = distancia;
+        this.premioEnDolares = premioEnDolares;
+        this.nombre = nombre;
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+        this.vehiculos = new ArrayList<Vehiculo>();
+        this.socorristaAuto = new SocorristaAuto();
+        this.socorristaMoto = new SocorristaMoto();
+    }
+
+    public Double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(Double distancia) {
+        this.distancia = distancia;
+    }
+
+    public Double getPremioEnDolares() {
+        return premioEnDolares;
+    }
+
+    public void setPremioEnDolares(Double premioEnDolares) {
+        this.premioEnDolares = premioEnDolares;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidadDeVehiculosPermitidos() {
+        return cantidadDeVehiculosPermitidos;
+    }
+
+    public void setCantidadDeVehiculosPermitidos(Integer cantidadDeVehiculosPermitidos) {
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public void darDeAltaAuto(double velocidad, double aceleracion, double anguloDeGiro, String patente) {
+        Auto auto = new Auto(velocidad, aceleracion, anguloDeGiro, patente);
+        agregarVehiculo(auto);
+    }
+
+    private void agregarVehiculo(Vehiculo vehiculo) {
+        if (this.vehiculos.size() < this.cantidadDeVehiculosPermitidos) {
+            this.vehiculos.add(vehiculo);
+        }else{
+            System.out.println("No hay Cupo en la Carrera, Limite: " + this.cantidadDeVehiculosPermitidos);
+        }
+    }
+
+    public void darDeAltaMoto(double velocidad, double aceleracion, double anguloDeGiro, String patente) {
+        Moto moto = new Moto(velocidad, aceleracion, anguloDeGiro, patente);
+        agregarVehiculo(moto);
+    }
+
+    public void eliminarVehiculo(Vehiculo vehiculo) {
+        this.vehiculos.remove(vehiculo);
+    }
+
+    public void eliminarVehiculoConPatente(String patente) {
+        Vehiculo v = this.buscarVehiculoPorPatente(patente);
+        if (v != null) {
+            this.eliminarVehiculo(v);
+        }
+
+    }
+
+    public Vehiculo getGanadador(){
+        Vehiculo ganador = null;
+        double maxValor = -Double.MAX_VALUE;
+        for (Vehiculo v : this.vehiculos) {
+            double valor = v.getVelocidad() * 0.5 * v.getAceleracion() / (v.getAnguloDeGiro() * (v.getPeso() - v.getRuedas() * 100));
+            if (valor > maxValor) {
+                maxValor = valor;
+                ganador = v;
+            }
+        }
+        return ganador;
+    }
+
+    public void socorrerAuto(String patente){
+        Auto auto = (Auto) this.buscarVehiculoPorPatente(patente);
+        if (auto != null) {
+            socorristaAuto.socorrer(auto);
+        }
+
+    }
+
+    public void socorrerMoto(String patente){
+        Moto moto = (Moto) this.buscarVehiculoPorPatente(patente);
+        if (moto != null) {
+            socorristaMoto.socorrer(moto);
+        }
+
+    }
+
+    private Vehiculo buscarVehiculoPorPatente(String patente) {
+        Vehiculo v = (Vehiculo) vehiculos.stream()
+                .filter(ve -> ve.getPatente().equals(patente))
+                .findFirst()
+                .orElse(null);
+        return v;
+    }
+}
---
+++ 1. Modulo Java/practica-integradora/dakar-carrera/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Carrera carrera = new Carrera(1000.0, 500.0, "Dakar", 5);
+
+        carrera.darDeAltaAuto(100.0, 200.0, 1.5, "A");
+        carrera.darDeAltaAuto(200.0, 300.0, 1.5, "B");
+        carrera.darDeAltaAuto(300.0, 400.0, 1.5, "C");
+
+        carrera.darDeAltaMoto(400.0, 500.0, 1.5, "D");
+        carrera.darDeAltaMoto(500.0, 600.0, 1.5, "E");
+        carrera.darDeAltaMoto(600.0, 700.0, 1.5, "F");
+
+        System.out.println(carrera.getGanadador().getPatente());
+
+    }
+}
