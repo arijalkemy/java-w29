d624a55e8a455c7560e454c11f75cbc0706e6d59
EFP210 <edwinpinilla125@gmail.com>
2025-01-13 10:19:37 -0500

    ejercicio productos

---
+++ 2. Modulo Spring/Morse/practica-excepciones/src/ejercicio1/PracticaExcepciones.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+public class PracticaExcepciones {
+    public Integer a = 0;
+    public Integer b = 300;
+    public PracticaExcepciones() {}
+
+    public Integer math(){
+        try{
+            return b/a;
+        } catch (Exception exception){
+            throw new IllegalArgumentException("No se puede dividir por 0");
+        }
+    }
+}
---
+++ 2. Modulo Spring/Morse/practica-excepciones/src/ejercicio2/Perecederos.java
@@ -0,0 +1,40 @@
+package ejercicio2;
+
+public class Perecederos extends Producto{
+    public int diasPorCaducar;
+
+    public Perecederos(String nombre, Double precio, int diasPorCaducar) {
+        super(nombre, precio);
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public String toString() {
+        return "Perecederos{" +
+                "precio=" + precio +
+                ", diasPorCaducar=" + diasPorCaducar +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    @Override
+    public Double Calcular(int CantidadDeProductos) {
+        if(diasPorCaducar == 1){
+            precio = precio / 4;
+        } else if(diasPorCaducar == 2){
+            precio = precio / 3;
+        } else if(diasPorCaducar == 3){
+            precio = precio / 2;
+        }
+
+        return precio * (CantidadDeProductos);
+    }
+}
