b75f24eaf9c0d90cc9ecf99af15327b5de6b3d7a
julicastro <castroejulian@gmail.com>
2025-01-14 13:06:11 -0300

    commit

---
+++ 2. Modulo Spring/ejercicio_global/src/main/java/com/bootcampW22/EjercicioGlobal/exception/DuplicateIdException.java
@@ -0,0 +1,7 @@
+package com.bootcampW22.EjercicioGlobal.exception;
+
+public class DuplicateIdException extends RuntimeException {
+    public DuplicateIdException(String message) {
+        super(message);
+    }
+}
