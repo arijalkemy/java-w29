6ade7b84026a93b0c2b008de88a006af8f35d73d
Eliana Denise Navarro <elianadnavarro@gmail.com>
2025-01-14 14:07:31 -0300

    Agrego ejercicio contador de calorias

---
+++ 2. Modulo Spring/4-contador-calorias/src/test/java/com/example/contador_calorias/ContadorCaloriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contador_calorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorCaloriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
