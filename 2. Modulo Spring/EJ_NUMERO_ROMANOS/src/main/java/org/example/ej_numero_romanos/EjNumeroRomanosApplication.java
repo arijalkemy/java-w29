8029ba055d6bacbbab0857e153d96206bcf6d4be
Juan Feo <juan.feo@mercadolibre.com.co>
2025-01-14 22:26:18 -0500

    Spring - Ejercicio numero romanos

---
+++ 2. Modulo Spring/EJ_NUMERO_ROMANOS/src/main/java/org/example/ej_numero_romanos/EjNumeroRomanosApplication.java
@@ -0,0 +1,13 @@
+package org.example.ej_numero_romanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjNumeroRomanosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjNumeroRomanosApplication.class, args);
+    }
+
+}
---
+++ 2. Modulo Spring/EJ_NUMERO_ROMANOS/src/test/java/org/example/ej_numero_romanos/EjNumeroRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ej_numero_romanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjNumeroRomanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
