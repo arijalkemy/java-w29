747fe7295c19c80edf3c06093f5a13c6bbdf0652
julianamroa <julianamroa@gmail.com>
2025-01-15 17:16:12 -0500

    Actividades Git

---
+++ 2. Modulo Spring/Spring-P1/ejercici1Spring/src/main/java/com/example/ejercici1Spring/Ejercici1SpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.ejercici1Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercici1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercici1SpringApplication.class, args);
+		System.out.println("Ejercici1SpringApplication");
+	}
+
+}
---
+++ 2. Modulo Spring/Spring-P1/ejercici1Spring/src/test/java/com/example/ejercici1Spring/Ejercici1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercici1Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercici1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
