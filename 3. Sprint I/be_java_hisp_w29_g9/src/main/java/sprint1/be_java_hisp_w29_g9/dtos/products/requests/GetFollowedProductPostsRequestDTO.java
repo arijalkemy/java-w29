6622198e22a6f41cc5c0c65124d45210d5d3d1b8
JhonSebasZ <zunigalopezjhonsebastian@gmail.com>
2025-01-27 13:43:55 -0500

    delete be_java_hisp_w29_g9

--- 3. Sprint I/be_java_hisp_w29_g9/src/main/java/sprint1/be_java_hisp_w29_g9/dtos/products/requests/GetFollowedProductPostsRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package sprint1.be_java_hisp_w29_g9.dtos.products.requests;
-
-import lombok.Getter;
-
-@Getter
-public class GetFollowedProductPostsRequestDTO {
-    private Integer user_id;
-    private String order;
-}
--- 3. Sprint I/be_java_hisp_w29_g9/src/main/java/sprint1/be_java_hisp_w29_g9/dtos/products/requests/GetPromoProductRequestDTO.java
+++
@@ -1,8 +0,0 @@
-package sprint1.be_java_hisp_w29_g9.dtos.products.requests;
-
-import lombok.Getter;
-
-@Getter
-public class GetPromoProductRequestDTO {
-    private Integer user_id;
-}
