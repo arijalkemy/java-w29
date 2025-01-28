f4946b4fc3e81c83d79fb4725e547c87bf27b819
Luis Manuel Jaimes Silva <luis.jaimes@mercadolibre.com.co>
2025-01-28 18:04:46 -0500

    Ejercicio de primer Sprint - grupo 5 - ejercicio "Social Media"

---
+++ 3. Sprint I/be_java_hisp_w29_g5/src/main/java/com/social_media/social_media/dto/response/PostPromoResponseDto.java
@@ -0,0 +1,25 @@
+package com.social_media.social_media.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PostPromoResponseDto {
+    private Long post_id;
+    private Long user_id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private ProductResponseDto product;
+    private Integer category;
+    private Double price;
+    private Boolean has_promo;
+    private Double discount;
+}
