620d6153953781fb09282d3ac05192b6d1152cee
Julian Castro <70999224+julicastro@users.noreply.github.com>
2025-01-30 11:13:05 -0300

    Update StudentNotFoundException.java

--- 4. Testing/ejercicio_obtener_diploma_test_unitarios/src/main/java/com/meli/obtenerdiploma/exception/StudentNotFoundException.java
+++ 4. Testing/ejercicio_obtener_diploma_test_unitarios/src/main/java/com/meli/obtenerdiploma/exception/StudentNotFoundException.java
@@ -7,4 +7,13 @@ public class StudentNotFoundException extends ObtenerDiplomaException {
     public StudentNotFoundException(Long id) {
         super("El alumno con Id " + id + " no se encuetra registrado.", HttpStatus.NOT_FOUND);
     }
+
+    /*
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    } */
 }
