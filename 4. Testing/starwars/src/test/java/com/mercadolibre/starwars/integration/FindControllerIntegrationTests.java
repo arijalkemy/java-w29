4be8174f3889353e089f99d3a96e704ae001708a
mdaneri98 <mdaneri98@gmail.com>
2025-01-31 15:15:48 -0300

    Added Integration tests on ´Starwars´ excercise

---
+++ 4. Testing/starwars/src/test/java/com/mercadolibre/starwars/integration/FindControllerIntegrationTests.java
@@ -0,0 +1,34 @@
+package com.mercadolibre.starwars.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.starwars.service.FindService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FindControllerIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void registerStudent_validStudent() throws Exception {
+        mockMvc.perform(get("/query"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
