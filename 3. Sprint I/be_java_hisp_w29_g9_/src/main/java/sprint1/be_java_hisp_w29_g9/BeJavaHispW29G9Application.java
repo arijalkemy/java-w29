ea696c78fdb7ef6e7ccca146b3c22b769ddc80ee
EFP210 <edwinpinilla125@gmail.com>
2025-02-05 08:32:52 -0500

    sprint 1 final

---
+++ 3. Sprint I/be_java_hisp_w29_g9_/src/main/java/sprint1/be_java_hisp_w29_g9/BeJavaHispW29G9Application.java
@@ -0,0 +1,13 @@
+package sprint1.be_java_hisp_w29_g9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW29G9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaHispW29G9Application.class, args);
+	}
+
+}
---
+++ 3. Sprint I/be_java_hisp_w29_g9_/src/main/java/sprint1/be_java_hisp_w29_g9/dtos/exception/ExceptionDto.java
@@ -0,0 +1,10 @@
+package sprint1.be_java_hisp_w29_g9.dtos.exception;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDto {
+    private String message;
+}
