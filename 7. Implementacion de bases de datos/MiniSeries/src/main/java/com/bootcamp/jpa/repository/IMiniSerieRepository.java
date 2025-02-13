9db30c749004b918ac33d4877be900bfe4ea7321
Angelo Daniel Padron <padron891@gmail.com>
2025-02-13 16:59:18 -0300

    add exercise

---
+++ 7. Implementacion de bases de datos/MiniSeries/src/main/java/com/bootcamp/jpa/repository/IMiniSerieRepository.java
@@ -0,0 +1,9 @@
+package com.bootcamp.jpa.repository;
+
+import com.bootcamp.jpa.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+}
