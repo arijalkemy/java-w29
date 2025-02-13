7a533225d82c3518f891f2bbbef3fbd9d377aa4d
Juan Feo <juan.feo@mercadolibre.com.co>
2025-02-13 15:42:26 -0500

    JPA + Hibernate - VIVO

---
+++ 7. Implementacion de bases de datos/EJ_MINISERIE/src/test/java/org/example/miniseries/MiniseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.miniseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniseriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
