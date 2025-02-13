22fd12a89764502291a4b1aeebad11e529541902
Eliana Denise Navarro <elianadnavarro@gmail.com>
2025-02-13 16:33:13 -0300

    ADD miniseries

---
+++ 7. Implementacion de bases de datos/MiniSeries/src/main/java/com/example/MiniSeries/model/MiniSerie.java
@@ -0,0 +1,22 @@
+package com.example.MiniSeries.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class MiniSerie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Double rating;
+
+    private int amount_of_awards;
+}
