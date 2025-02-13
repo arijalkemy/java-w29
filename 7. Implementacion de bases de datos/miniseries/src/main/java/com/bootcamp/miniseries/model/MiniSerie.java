98ca9e65fd36fc311d7aef26a257bb6661e35282
Daniel Sebastian Ochoa Urrego <danielsebastian.ochoa@mercadolibre.com.co>
2025-02-13 14:47:51 -0500

    uploading first jpa miniseries exercise

---
+++ 7. Implementacion de bases de datos/miniseries/src/main/java/com/bootcamp/miniseries/model/MiniSerie.java
@@ -0,0 +1,19 @@
+package com.bootcamp.miniseries.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Double rating;
+    private int ammount_of_awards;
+}
