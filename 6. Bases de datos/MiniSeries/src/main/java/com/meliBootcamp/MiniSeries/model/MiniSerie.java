1cd9b807b13a6c2930bdeeb4f3fdd68b8dc3bc98
Christian Bravo Reyes <cbravor@fi.uba.ar>
2025-02-13 16:22:51 -0300

    agrego ejercicio Configurando JPA

---
+++ 6. Bases de datos/MiniSeries/src/main/java/com/meliBootcamp/MiniSeries/model/MiniSerie.java
@@ -0,0 +1,22 @@
+package com.meliBootcamp.MiniSeries.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Double rating;
+
+    private int amount_of_awards;
+
+}
