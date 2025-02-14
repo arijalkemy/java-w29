abe7cbb33195280f3b4a94e019f03ed049ad547c
FelipeLL <f3lip3.3z@hotmail.com>
2025-02-14 07:26:21 -0500

    feat(impl_bases_datos): add miniseries exercise

---
+++ 7. Implementacion de bases de datos/MiniSeries/src/main/java/exercise/miniseries/model/MiniSerie.java
@@ -0,0 +1,16 @@
+package exercise.miniseries.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Double rating;
+    private Integer amount_of_awards;
+}
