a5cfd1d260d69276ea368299580d13b5b288cf33
Christian Bravo Reyes <cbravor@fi.uba.ar>
2025-02-14 15:23:28 -0300

    elimino ejercicio MiniSeries de modulo 6.Bases de datos

--- 6. Bases de datos/MiniSeries/src/main/java/com/meliBootcamp/MiniSeries/repository/IMiniSerieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meliBootcamp.MiniSeries.repository;
-
-import com.meliBootcamp.MiniSeries.model.MiniSerie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IMiniSerieRepository extends JpaRepository<MiniSerie, Long> {
-}
