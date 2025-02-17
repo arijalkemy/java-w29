0e1dc05287fdc5b10e0591e23467ab644db4d8a5
Sebastián Chacón <sebastianchaconbenitez@gmail.com>
2025-02-17 08:22:22 -0500

    add: exercises

---
+++ 7. Implementacion de bases de datos/Impl-BD-Vivo/src/main/java/com/example/implbdvivo/ImplBdVivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.implbdvivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplBdVivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplBdVivoApplication.class, args);
+    }
+
+}
---
+++ 7. Implementacion de bases de datos/Impl-BD-Vivo/src/test/java/com/example/implbdvivo/ImplBdVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implbdvivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplBdVivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
