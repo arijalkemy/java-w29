7069f0b63c58f735213541684b8992899ea96963
faaguero_meli <fabiana.aguero@mercadolibre.com>
2025-02-26 18:24:46 -0300

    Subo ejercicio transacciones

---
+++ 1. Modulo Java/Carrera/abstractasEInterfaces/src/main/java/com/ejercicios/abstractaseinterfaces/AbstractasEInterfacesApplication.java
@@ -0,0 +1,11 @@
+package com.ejercicios.abstractaseinterfaces;
+
+import org.springframework.boot.SpringApplication;
+
+public class AbstractasEInterfacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbstractasEInterfacesApplication.class, args);
+    }
+
+}
---
+++ 1. Modulo Java/Carrera/abstractasEInterfaces/src/test/java/com/ejercicios/abstractaseinterfaces/AbstractasEInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicios.abstractaseinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractasEInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
